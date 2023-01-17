458e093317a73382b1a5e32866aac851a4d258e9
eitanpushett <95567545+eitanpushett@users.noreply.github.com>
2023-01-17 14:49:54 +0200

    Update dani.py

--- dani.py
+++ dani.py
@@ -1,7 +1,6 @@
 import os
 path = "my-treerd-folder"
 print(os.environ['CNVRG_JOB_ID'])
-os.chdir('output')
 os.mkdir(path)
 for i in range (1, 10000):
     f = open("{}/demofile{}.txt".format(path, i), "a")
